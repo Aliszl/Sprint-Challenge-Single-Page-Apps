7a19c694777cfe5f7499abb246bb355a6c1c07cd
Alison Law <alisparklaw@gmail.com>
2019-11-15 10:23:03 +0000

    box styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,14 +1,29 @@
 import React from "react";
 import CharacterList from "./CharacterList";
+import styled from "styled-components";
+
+// styling
+const StyledDiv = styled.div`
+  margin: 20px auto;
+  width: 60%;
+  border: grey 2px solid;
+  height: auto;
+  border-radius: 4px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  overflow: hidden;
+  &:hover {
+    background-color: skyblue;
+  }
+`;
 
 const CharacterCard = props => {
   return (
-    <div>
+    <StyledDiv>
       <h1>{props.name}</h1>
       <img src={props.url} alt={props.name} />
       <h3>Status: {props.status}</h3>
       <h3>Species: {props.species}</h3>
-    </div>
+    </StyledDiv>
   );
   // ;<span>todo: character</span>;
 };
